0dc92c578bf7f2adaaab81706e53af425ba8542b
ragul66 <114981373+ragul66@users.noreply.github.com>
2024-02-02 20:49:26 +0530

    Name Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <div>
-        Hello!! B-Hub
-      </div>
-    </>
+    <div className='text-2xl w-full h-full bg-gray-900 text-white'>
+      B-Hub Consultancy & Services
+    </div>
   )
 }
 
