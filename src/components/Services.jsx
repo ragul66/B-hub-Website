18e62f2fe06c1de1b818c215646d9d2d0a65f599
ragul66 <114981373+ragul66@users.noreply.github.com>
2024-02-04 17:52:36 +0530

    services updated in image1

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,13 +1,14 @@
 
+// import Webd from "../assets/webd.jpg"
 
 const Services = () => {
+
     const data = [
         {
-            Heading: `Hello Services`
-        },
-        {
-            Heading: `Hello Services`
+            Heading: `Hello Services`,
+            img: "src/assets/webd.jpg"
         }
+
     ]
     return (
 
@@ -16,11 +17,12 @@ const Services = () => {
                 <div className=" sm: font-primary sm: text-[25px] sm: text-textcolor-0 sm: -mt-80 sm: text-center lg:font-primary lg:text-[50px] lg:text-textcolor-0 lg:-mt-32 lg:text-center" >Services</div>
                 <div>
                     <div>
-                        {data.map((d) => {
+                        {data.map((d) => (
                             <div className="lg:font-primary lg:text-textcolor-0">
-                                <p>{d.Heading}</p>
+                                <img className="" src={d.img} alt="image" />
+                                <p className="">{d.Heading}</p>
                             </div>
-                        })}
+                        ))}
                     </div>
 
                 </div>
