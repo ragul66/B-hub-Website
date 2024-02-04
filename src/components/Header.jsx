659ed27adfe2590ffd92170c1590fc81e0ff0343
ragul66 <114981373+ragul66@users.noreply.github.com>
2024-02-04 11:09:43 +0530

    commit header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <>
             <div className="sm: bg-textcolor-0 sm: w-full sm: font-primary sm: rounded-b-[30px] sm: h-8 sm: text-center lg:bg-textcolor-0 lg:w-[100%] lg:h-16 lg:font-primary lg:text-[25px] lg:text-center ">
-                <p className="">Strive not to be a success, but rather to be of value.</p>
+                <p className=""><q>Strive not to be a success, but rather to be of value.</q></p>
             </div>
         </>
     )
