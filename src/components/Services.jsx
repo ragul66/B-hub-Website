451bca456431e97366093acceed54627c8b5e0c4
ragul66 <114981373+ragul66@users.noreply.github.com>
2024-02-04 20:18:27 +0530

    Commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -19,8 +19,8 @@ const Services = () => {
                     <div>
                         {data.map((d) => (
                             <div className="lg:font-primary lg:text-textcolor-0">
-                                <img className="" src={d.img} alt="image" />
-                                <p className="">{d.Heading}</p>
+                                <img className="lg:rounded-3xl lg:w-[500px] lg:mt-12" src={d.img} alt="image" />
+                                <p className="lg:text-right">{d.Heading}</p>
                             </div>
                         ))}
                     </div>
