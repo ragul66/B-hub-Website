a7dec949841dd71027a03945774e90c9e9ff49ed
ragul66 <114981373+ragul66@users.noreply.github.com>
2024-02-09 03:57:50 +0530

    commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -92,11 +92,11 @@ const About = () => {
                 <Header />
                 <div className='font-primary'>
                     <h1 className='mt-12 text-[25px] text-center'><q>Elevate Your Digital Future with Our IT Expertise</q></h1>
-                    <h1 className='text-[100px] text-center mt-36'>We Are Creative And Phenomenal</h1>
+                    <h1 className='text-[80px] text-center mt-36'>We Are Creative And Phenomenal</h1>
                     <p className=' text-center text-[20px] ml-2 mr-2'><q>At B-Hub, we offer a range of professional services to meet various digital needs. We specialize in Web designing, Web development, and UI/UX design, combining creativity and technical skills to <br /> create visually appealing and highly functional solutions. Our expertise also extends to media works, where we focus on producing impactful visual content. Additionally, we actively collaborate on college projects, supporting the educational and professional growth of students. Whether you need a beautiful website, a seamless user experience, compelling media content, or assistance with academic projects, our dedicated team is committed to exceeding expectations and delivering excellence in every project.</q></p>
 
                     <div>
-                        <Marquee speed={300} gradient={false} className="mt-[85px] text-[200px]">
+                        <Marquee speed={300} gradient={false} className="mt-[125px] text-[100px]">
                             <div className="announcement">
                                 <div className="announcement-text text1">
                                     <span>Our vision is to be the trusted partner for businesses,  We aim to simplify complexity, empower growth, and ensure our clients stay ahead in the ever-evolving digital landscape,</span>
@@ -112,7 +112,7 @@ const About = () => {
                         <div>
                             <p className='text-center text-[30px] ml-12 mt-12 animate-fade-up'>Create Positive Goals in your life & <br /> reach them as happy as you can</p>
                         </div>
-
+                        <h1 className="border-b-2 text-textcolor-0"></h1>
                         <h1 className='text-center text-[70px] mt-12'>Our Team</h1>
 
                         <div className='space-x-6 grid grid-cols-4 grid-flow-row p-6 animate-fade-in-up'>
@@ -127,7 +127,7 @@ const About = () => {
                     </div>
 
                     {/* Team Mates Card Ends */}
-
+                    <h1 className="border-b-2 text-textcolor-0"></h1>
                     {/* Projects section starts */}
                     <div>
                         <h1 className='text-center text-[70px]'>Projects</h1>
