b748be70c28105e33622ccca89b978c7e2041902
ragul66 <114981373+ragul66@users.noreply.github.com>
2024-02-09 04:10:36 +0530

    Alignment started

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,54 +2,49 @@ import webd from "../assets/webd.jpg";
 import adminimg from "../assets/adminimg.png";
 import mediaimg from "../assets/mediaimg.webp";
 import contentimg from "../assets/contentimg.jpg";
+import '../index.css'
 
 const Services = () => {
   return (
-    <div className="services-container">
-      <div className="title lg:text-6xl lg:text-textcolor-0 lg:text-center">
+    <div className="container">
+      <div className="title lg:text-6xl lg:text-textcolor-0 lg:text-center lg:-mt-40 lg:text-[70px]">
         Services Provided
       </div>
 
-      <ServiceItem
-        imgSrc={webd}
-        imgAlt="Web Development"
-        description="We specialize in MERN and PERN stack development, offering professional front-end and back-end services. Our focus includes robust security measures and regular maintenance for enhanced performance and secure transactions."
-      />
+      <div className="service-items-container">
+        <ServiceItem
+          imgSrc={webd}
+          imgAlt="Web Development"
+          description="We specialize in MERN and PERN stack development, offering professional  front-end and back-end services. Our focus includes robust security measures and regular maintenance for enhanced performance and secure transactions"
+        />
 
-      <ServiceItem
-        imgSrc={adminimg}
-        imgAlt="Admin Services"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure velit facere laborum sint non molestiae dolore culpa aut est magnam, ea molestias odio amet, hic eos labore eaque reprehenderit optio."
-        alignRight
-      />
+        <ServiceItem
+          imgSrc={adminimg}
+          imgAlt="Admin Services"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure velit facere laborum sint non molestiae dolore culpa aut est magnam, ea molestias odio amet, hic eos labore eaque reprehenderit optio."
+        />
 
-      <ServiceItem
-        imgSrc={mediaimg}
-        imgAlt="Media Content Creation"
-        description="The main objective of media content creation is to design and create flyers, posters, and ID card designs that will be unique and creative."
-      />
+        <ServiceItem
+          imgSrc={mediaimg}
+          imgAlt="Media Content Creation"
+          description="The main objective of media content creation is to design and create flyers, posters, and ID card designs that will be unique and creative."
+        />
 
-      <ServiceItem
-        imgSrc={contentimg}
-        imgAlt="Content Creation"
-        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti rerum vitae doloremque quam dignissimos sunt, minus reiciendis unde ut ipsam et ab in. Cum, in corrupti accusamus molestias soluta voluptates."
-        alignRight
-      />
+        <ServiceItem
+          imgSrc={contentimg}
+          imgAlt="Content Creation"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti rerum vitae doloremque quam dignissimos sunt, minus reiciendis unde ut ipsam et ab in. Cum, in corrupti accusamus molestias soluta voluptates."
+        />
+      </div>
     </div>
   );
 };
 
-const ServiceItem = ({ imgSrc, imgAlt, description, alignRight }) => {
-  const imageClass = `lg:w-96 lg:rounded-3xl lg:mt-48 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 transform transition-all hover:translate-x-4 ${alignRight ? "lg:ml-96" : "lg:ml-16"
-    }`;
-
-  const textClass = `lg:font-primary lg:text-textcolor-0 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 ${alignRight ? "lg:text-right lg:mr-28" : "lg:ml-72"
-    } lg:-mt-44`;
-
+const ServiceItem = ({ imgSrc, imgAlt, description }) => {
   return (
     <div className="service-item animate-fade-in-up">
-      <img className={imageClass} src={imgSrc} alt={imgAlt} />
-      <p className={textClass}>{description}</p>
+      <img className="service-item-image" src={imgSrc} alt={imgAlt} />
+      <p className="service-item-description">{description}</p>
     </div>
   );
 };
