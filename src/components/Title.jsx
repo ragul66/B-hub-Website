91db70b9e0461910c9d1ea5b8c0b91d7dc533cb9
ragul66 <114981373+ragul66@users.noreply.github.com>
2024-02-15 22:02:58 +0530

    commit

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ const Title = () => {
   return (
     <>
       <div className=" text-textcolor-0 lg:bg-background-0 lg:w-full lg:h-screen ">
-        <div className="mt-48  text-textcolor-0 sm: font-primary   lg:font-primary lg:mt-0">
+        <div className="mt-56  text-textcolor-0 sm: font-primary   lg:font-primary lg:mt-0">
           <p className="text-[50px] align-middle text-center mt-20 animate-fade-in-up md:text-[90px]  lg:align-middle lg:text-textcolor-0   lg:text-[150px] lg:text-center lg:mt-28 lg:animate-fade-in-up">
             B-Hub
           </p>
